225fa0c83d965b3530bcce9bb8673c4861a1e6d5
DLAcoding <andujar_dani@hotmail.com>
2017-06-27 14:10:19 +0200

    Update handlers_csv.py

--- handlers_csv.py
+++ handlers_csv.py
@@ -9,13 +9,29 @@ def clean_empty(lista):
             c.append(a)
     return c
 
-#Delete the ";" and makes the file accesible
+#Delete the "comma" and makes the file accesible. By default if the char is not specified ";" is what deletes. 
 
-def fix_file(lista):
+def fix_file(lista,sep=";"):
     c = list()
     for a in lista:
-        b = a[0].split(";")
+        b = a[0].split(sep)
         c.append(b)
-    return c
+    return c    
 
+#WITH THE FILE FIX AND FIRST LINE IS THE HEADERS....
+
+#Return number of columns with header
+
+def max_column(file):    
+    return len(file[0])
+
+#Return number of rows
+
+def max_row(file):
+    return len(file)
+
+#Inserts a new column in a specific position. If the position is not specified it's added at the beginning.
+
+def insert_col(file,data,pos=0):    
+    file[0].insert(pos,data)
 
